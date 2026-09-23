916f7278ca70fe89fbb9d340845ec9152610648e
Satvik Nandal <satvik@chathealth.com.au>
2026-09-23 18:37:20 +0800

    web design banner fix

--- src/components/uiux/UIUXHero.tsx
+++ src/components/uiux/UIUXHero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import webuiuxbg from "../../assets/hero/webuiuxbg.svg";
 import uxbannerbg from "../../assets/hero/uxbg.webp";
-import { PaletteIcon } from "lucide-react";
+import { PaletteIcon, Smartphone } from "lucide-react";
 
 interface UIUXHeroProps {
   children?: React.ReactNode;
@@ -10,29 +10,25 @@ interface UIUXHeroProps {
 const UIUXHero: React.FC<UIUXHeroProps> = () => {
 
     const DigitalBanner = () => {
-        return <div className='py-12 relative h-[600px] overflow-hidden flex flex-col justify-center radialgradient'>
-        <div className='container-wrapper-transparent z-10 relative pl-6 '>
-            {/* <label className='font-normal text-white text-sm ml-1'>Website Design & UX</label> */}
-            <div className="mb-2 md:mb-5 inline-flex items-center gap-2 rounded-full border border-blue-400/15 bg-blue-300/10 px-3 md:px-4 py-2 text-xs md:text-sm font-medium text-blue-200">
-            <div className="flex w-5 h-5 md:h-7 md:w-7 items-center justify-center rounded-full bg-blue-400/20">
-                <PaletteIcon/>
+        return <div className='py-6 md:py-12 relative h-[350px] md:h-[600px] overflow-hidden flex flex-col md:justify-center radialgradient'>
+        <div className='container-wrapper-transparent z-10 relative md:pl-6 '>
+            <div className="mb-4 md:mb-5 inline-flex items-center gap-2 rounded-full border border-blue-400/15 bg-blue-400/10 px-3 md:px-4 py-2 text-xs md:text-sm font-medium text-blue-200">
+                <div className="flex w-5 h-5 md:h-7 md:w-7 items-center justify-center rounded-full bg-blue-500/20">
+                <PaletteIcon size={15} />
+                </div>
+                Website Design & UX
             </div>
-            Website Design & UX
+            <h1 className="max-w-3xl text-2xl font-medium leading-[1.08] tracking-tight text-white sm:text-4xl lg:text-5xl">
+                Elevating {" "}
+                <span className='mx-1 bg-[linear-gradient(84deg,#3ca1ff_0%,#6e24fb_30%,#c61ee8_60%,#ff6948_100%)] bg-clip-text font-medium text-transparent pb-5'>Forward Thinking</span>
+                <br/> User Experience
+            </h1>
+            <p className="mt-4 md:mt-6 max-w-xl text-[14px] md:text-base leading-5.5 md:leading-6.5 text-white">
+                Crafting websites that elevate your brand and enhance user experience. We specialise in designing custom websites that are not only visually compelling but also strategically aligned with your business objectives. Our team combines creativity with functionality to deliver websites that captivate audiences and convert visitors into loyal customers.
+            </p>
         </div>
-            <h1 className='max-w-3xl text-2xl font-normal leading-[1.08] tracking-tight text-white sm:text-4xl lg:text-5xl'>
-            Elevating
-            <span className='mx-2 bg-[linear-gradient(84deg,#3ca1ff_0%,#6e24fb_30%,#c61ee8_60%,#ff6948_100%)] bg-clip-text font-medium text-transparent pb-5'>Forward Thinking</span>
-            <br/> User Experience</h1>
-            <p className='mt-6 max-w-xl text-base leading-6 text-gray-200'>Crafting websites that elevate your brand and enhance user experience. We specialise in designing custom websites that are not only visually compelling but also strategically aligned with your business objectives. Our team combines creativity with functionality to deliver websites that captivate audiences and convert visitors into loyal customers.</p>
-        </div>
-        {/* <GradientDots duration={20} className='opacity-50' /> */}
-        <img
-            src={webuiuxbg}
-            className="absolute -top-10 z-[2] w-full opacity-20"
-            alt=""
-        />
-        {/* <div className="absolute z-[1] h-full w-full bg-gradient-to-br from-[#8adcffe3] to-[#fac9ffe3]" /> */}
-        <img src={uxbannerbg} className='w-full absolute top-0 opacity-50'/>
+        <img src={webuiuxbg} className="absolute h-full md:h-auto object-cover md:object-fill -top-[0px] z-[2] opacity-30" alt="" />
+        <img src={uxbannerbg} className='w-auto h-full md:w-full md:h-auto absolute top-0 opacity-50'/>
         </div>
     }
 
